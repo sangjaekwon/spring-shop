7c1b194f1e59d6392572fe45bb37b5fb1704a145
sangjaekwon <sjsj00718@gmail.com>
2025-07-23 19:40:25 +0900

    회원, 상품, 주문 도메인 개발 및 테스트 작성 완료

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockExcpetion;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,8 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
 @DiscriminatorColumn(name = "dtype")
-@Getter
-@Setter
+@Getter @Setter
 public abstract class Item {
 
     @Id
@@ -26,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockExcpetion("need more Stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
---
+++ src/main/java/jpabook/jpashop/exception/NotEnoughStockExcpetion.java
@@ -0,0 +1,18 @@
+package jpabook.jpashop.exception;
+
+public class NotEnoughStockExcpetion extends RuntimeException {
+    public NotEnoughStockExcpetion(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockExcpetion(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockExcpetion(Throwable cause) {
+        super(cause);
+    }
+
+    public NotEnoughStockExcpetion() {
+    }
+}
