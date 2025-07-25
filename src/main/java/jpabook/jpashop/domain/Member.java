c248fe87e6023fa8e39ce4ce613a3ace69b83865
sangjaekwon <sjsj00718@gmail.com>
2025-07-25 19:27:20 +0900

    기초 API 및 샘플 데이터 입력 완료

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -1,6 +1,8 @@
 package jpabook.jpashop.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,6 +23,7 @@ public class Member {
 
     @Embedded
     private Address address;
+
 
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
